a5d99b5d8625e18fa4a9d17a55be7f6fd9cc3266
Anthony Campbell <anthony@claydonkey.com>
2014-04-17 10:36:45 +0100

    Import barcode4j

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import play.Project._
+
 name := "ean"
 
 version := "1.0-SNAPSHOT"
@@ -5,7 +7,8 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "net.sf.barcode4j" % "barcode4j" % "2.1"
 )     
 
-play.Project.playScalaSettings
+playScalaSettings
--- project/Build.scala
+++ project/Build.scala
@@ -1,16 +1,18 @@
 import sbt._
-import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "ean-scala"
-  val appVersion      = "1.0"
+  val appName = "ean"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     "net.sf.barcode4j" % "barcode4j" % "2.1"
   )
+
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.sonatypeRepo("snapshots")
+
+
+
   )
+
 }
